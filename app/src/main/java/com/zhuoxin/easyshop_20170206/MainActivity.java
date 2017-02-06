b1595a7f8e1e01b135d1c95dab63e5614b1c72fc
e <agang@ganggang.com>
2017-02-06 22:40:07 +0800

    测试，更新

--- app/src/main/java/com/zhuoxin/easyshop_20170206/MainActivity.java
+++ app/src/main/java/com/zhuoxin/easyshop_20170206/MainActivity.java
@@ -1,13 +1,15 @@
-package com.zhuoxin.easyshop_20170206;
+ package com.zhuoxin.easyshop_20170206;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "大家好", Toast.LENGTH_SHORT).show();
     }
 }
